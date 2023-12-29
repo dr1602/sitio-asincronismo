d25799a88e9b11343edaecae6582675f39e87072
dr1602 <david.arangelg@gmail.com>
2023-12-29 15:03:36 -0600

    API implementation

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,29 +10,35 @@ const options = {
 	}
 };
 
+async function fetchData(urlAPI) {
+    const response = await fetch(urlAPI, options)
+    const data = await response.json();
+    return data;
+}
+
 (async () => {
     try {
         const videos = await fetchData(url);
         let view = `
             ${videos.items.map(video => `
 
-            <div class="group relative">
+            <div class="group relative cursor-pointer">
 
                 <div
                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src='${video.snippet.thumbnail.high.url}' alt='${video.snippet.description}' class="w-full">
+                    <img src='${video.snippet.thumbnails.high.url}' alt='${video.snippet.description}' class="w-full">
                 </div>
 
                 <div class="mt-4 flex justify-between">
-                    <h3 class="text-slate-600 mt-3 text-base sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 select-none">
+                    <h3 class="text-slate-600 group-hover:text-slate-300 mt-3 text-base sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 select-none">
                         <span aria-hidden="true" class="absolute inset-0"></span>
-                        ${video.snippet.tittle}
+                        ${video.snippet.title}
                     </h3>
                 </div>
 
             </div>
 
-            `).slice(0,3).join('')}            
+            `).slice(0,6).join('')}            
         `;
         content.innerHTML = view;
     } catch (error) {
@@ -46,10 +52,4 @@ const options = {
 // 	console.log(result);
 // } catch (error) {
 // 	console.error(error);
-// }
-
-// async function fetchData(urlAPI) {
-//     const response = await fetch(urlAPI, options)
-//     const data = await response.json();
-//     return data;
 // }
